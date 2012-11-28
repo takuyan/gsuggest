4c9166bf4a3135fb8ce0a781d57641d35cd5f1ae
takuyan <fanciful.dream@gmail.com>
2012-11-29 00:20:12 +0900

    bugfix for Invalid byte sequence in utf-8

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 require 'nokogiri'
 require 'open-uri'
+require 'kconv'
 
 class MainController < ApplicationController
 
@@ -10,15 +11,23 @@ class MainController < ApplicationController
   end
 
   def search
-    words = %w"あ い う え お か き く け こ さ し す せ そ た ち つ て と な に ぬ ね の は ひ ふ へ ほ ま み む め も や ゆ よ わ を ん"
     @q = params[:q]
     @suggestions = []
+
+    single_query = "#{@q}"
+    single_url = "http://google.co.jp/complete/search?output=toolbar&q=#{URI.encode(single_query)}"
+    single_doc = Nokogiri::XML(open(single_url).read)
+    single_doc.xpath("//suggestion").each do |e|
+      @suggestions << e.attributes['data'].to_s.toutf8 #.to_s.encode("UTF-8", "UTF-8", invalid: :replace, undef: :replace, replace: '.')
+    end
+
+    words = %w"あ い う え お か き く け こ さ し す せ そ た ち つ て と な に ぬ ね の は ひ ふ へ ほ ま み む め も や ゆ よ わ を ん"
     words.each do |w|
       query = "#{@q} #{w}"
       url = "http://google.co.jp/complete/search?output=toolbar&q=#{URI.encode(query)}"
       @doc = Nokogiri::XML(open(url).read)
       @doc.xpath("//suggestion").each do |e|
-        @suggestions << e.attributes['data']
+        @suggestions << e.attributes['data'].to_s.toutf8 #.to_s.encode("UTF-8", "UTF-8", invalid: :replace, undef: :replace, replace: '.')
       end
     end
   end
