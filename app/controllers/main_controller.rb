548895e2f076a01358c43202a3c21686aacbbe9a
takuyan <fanciful.dream@gmail.com>
2014-08-19 06:18:35 +0900

    not cached search action

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,7 @@ require 'kconv'
 
 class MainController < ApplicationController
 
-  caches_action :index, :search, expires_in: 1.month
+  caches_action :index, expires_in: 1.month
 
   def index
   end
